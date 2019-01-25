d98ab8b0639afdadd435616c3604cb610b85dc97
Walter <walter.damiata@community.unipa.it>
2019-01-25 18:00:47 +0100

    File attuali

---
+++ symb_tab.h
@@ -0,0 +1,33 @@
+#define HASHSIZE 101
+
+typedef struct c{
+    char *nome;
+    char *codice;
+    char *squadra;
+    char *pettorina;
+    ciclista *next;
+}ciclista;
+
+typedef struct t{
+    char *data;
+    char *citta_p;
+    char *citta_a;
+    supporto *s;
+    tratto *next;
+}tratto;
+
+typedef struct s{
+    int tempo_tratto;
+    int tempo_totale;
+    char pettorina;
+    ciclista *c;
+    supporto *next;
+}supporto;
+
+ciclista *hashtable[HASHSIZE];
+
+unsigned int hash(char *p);
+ciclista *lookup(char *p);
+ciclista *ins_ciclista(char *codice, char *nome, char *squadra, char *pettorina);
+tratto *ins_tratto(char *data, char *citta_p, char *citta_a);
+supporto *ins_supporto(char *pettorina, int tempo);
